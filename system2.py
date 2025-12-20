9600991eb66b10b046837f182c11b05ca586f0e8
YJC714 <yunjungchang714@gmail.com>
2025-12-20 10:46:04 +0800

    Update system2.py

--- system2.py
+++ system2.py
@@ -673,7 +673,11 @@ elif st.session_state.view == "運動場地":
                 
                 with col2:
                     
-                    map_url = f"https://www.google.com.tw/maps/search/{p['name']}"
+                    
+                    if p['name']=="成大銀髮健身俱樂部":
+                        map_url = f"https://www.google.com.tw/maps/search/北區仁愛里活動中心}"
+                    else:
+                        map_url = f"https://www.google.com.tw/maps/search/{p['name']}"
                     
                     # 讓按鈕垂直置中（加一點空間）
                     st.write("") 
